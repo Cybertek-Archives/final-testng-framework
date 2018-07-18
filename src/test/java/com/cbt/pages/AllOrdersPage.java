e13febaa35eaf723cbec456567b15c202e26494e
CybertekSchool <github@cybertekschool.com>
2018-07-18 19:05:36 -0400

    checking in missing file

--- src/test/java/com/cbt/pages/AllOrdersPage.java
+++ src/test/java/com/cbt/pages/AllOrdersPage.java
@@ -1,6 +1,5 @@
 package com.cbt.pages;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
